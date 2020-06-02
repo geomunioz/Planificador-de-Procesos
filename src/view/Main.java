1315c4d3b5d1d4896faab97d1b34bacbaa48e248
jorgetmz@outlook.com <jorgetmz@outlook.com>
2020-06-01 22:24:37 -0500

    Actualizar

--- src/view/Main.java
+++ src/view/Main.java
@@ -29,22 +29,22 @@ public class Main {
         listaFCFS.add(new Proceso("E", 4, 1));
         listaFCFS.add(new Proceso("F", 5, 4));
         listaFCFS.add(new Proceso("G", 6, 3));*/
-        /* PRUEBA FCFS 
-        metodo = manejador.firstComeFirtServed(listaFCFS);
-        System.out.println(metodo);
+        /* PRUEBA FCFS */
+        /*metodo = manejador.firstComeFirtServed(listaFCFS);
+        System.out.println(metodo);*/
         
         
         
         /* PRUEBA Prioridad */
         Manejador manejador2 = new Manejador();
         List<Proceso> listaP = new ArrayList();
-        listaP.add(new Proceso("A", 0, 2, 2));
-        listaP.add(new Proceso("B", 1, 3, 3));
-        listaP.add(new Proceso("C", 2, 4, 4));
-        listaP.add(new Proceso("D", 3, 3, 2));
-        listaP.add(new Proceso("E", 4, 1, 0));
-        listaP.add(new Proceso("F", 5, 4, 3));
-        listaP.add(new Proceso("G", 6, 3, 1));
+        listaP.add(new Proceso("P1", 0, 10, 3));
+        listaP.add(new Proceso("P2", 0, 1, 1));
+        listaP.add(new Proceso("P3", 0, 2, 3));
+        listaP.add(new Proceso("P4", 0, 1, 4));
+        listaP.add(new Proceso("P5", 0, 5, 2));
+        //listaP.add(new Proceso("F", 5, 4, 3));
+        //listaP.add(new Proceso("G", 6, 3, 1));
         /* PRUEBA FCFS */
         metodo = manejador2.priority(listaP);
         System.out.println(metodo);
